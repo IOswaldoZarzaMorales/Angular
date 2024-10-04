326cdbe69b0d9f5ffc5d58f783192795b0879df7
IOswaldoZarzaMorales <oswaldomorales26@hotmail.com>
2024-10-04 03:23:22 -0600

    primeros-componentes

---
+++ clase-02-componentes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    SideBarComponent,
+    MainWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
